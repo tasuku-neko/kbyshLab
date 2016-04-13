f396b930b5cb5d2443f48367e0f9405dce65698d
Yugo Morimura <arimaron.mohumohu@gmail.com>
2016-04-13 16:10:16 +0900

    jankenの関数とテンプレを追加

---
+++ janken.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+//出す手は数値で返す(0:ぐー,1:ちょき,2:ぱー)
+int getEnemyHand();
+int getMyHand();
+//勝ち負けは数値で返す(0:勝ち,1:負け)
+int getVs(int,int);
+void printResult();
+
+
+int main(){
+}
+
+
+
