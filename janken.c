523781bf3514c7790112e62dad861fa11e92bf4e
KoukiMotoyama <g1345369@cc.kyoto-su.ac.jp>
2016-04-13 17:25:22 +0900

    勝ち負けの判定

--- janken.c
+++ janken.c
@@ -12,5 +12,35 @@ void printResult();
 int main(){
 }
 
+int gitVs(per,com){
+	int res;//勝ち負け
 
+//あいこの場合
+	if(per == com){
+		res = 2;
+
+//その他
+		else{
+			if(per == 0 && com == 1){
+				res = 0;
+			}
+			if(per == 0 && com == 2){
+				res = 1;
+			}
+			if(per == 1 && com == 0){
+				res = 1
+			}
+			if(per == 1 && com == 2){
+				res = 0;
+			}
+			if(per == 2 && com == 0){
+				res = 0
+			}
+			if(per == 1 && com == 2){
+				res = 1;
+			}
+		}
+	}
+	return res;
+}
 
